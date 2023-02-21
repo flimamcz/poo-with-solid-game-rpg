1dcd0bf6b436f2b2537e778518f5a6913246cc05
Filipe Lima <cimofelipe4@gmail.com>
2023-02-21 16:15:57 -0300

    feat: init project + requirement 01 [OK]

---
+++ src/Races/Race.ts
@@ -0,0 +1,16 @@
+abstract class Race {
+  readonly name: string;
+  readonly dexterity: number;
+  constructor(private _name: string, private _dexterity: number) { 
+    this.name = _name;
+    this.dexterity = _dexterity;
+  }
+
+  static createdRacesInstances(): number | string {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+} 
+
+export default Race;
